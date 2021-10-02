907668f84266307235b3e01d436728787f01f011
Lab-Game <eric.lehman@gmail.com>
2021-10-01 20:38:46 -0700

    add display file

--- main.js
+++ main.js
@@ -4,11 +4,14 @@ const boardSize = 32;
 function init() {
   console.log("Init Called");
 
-  // Create the game board.
+  // Create the game board
   for (var row = 0; row < boardSize; row++) {
     board.push([]);
     for (var col = 0; col < boardSize; col++) {
       board[row].push(new Square(row, col));
     }
   }
+
+  // Display board
+  updateDislpay();
 }
