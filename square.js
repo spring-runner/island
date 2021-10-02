bf2f0932899b95b345831ac5158395feb5b39a73
Lab-Game <eric.lehman@gmail.com>
2021-10-01 21:53:52 -0700

    starting island is smoother

--- square.js
+++ square.js
@@ -1,5 +1,6 @@
 function Square(row, col) {
   this.row = row;
   this.col = col;
-  this.elevation = Math.floor(Math.random() * 8 - 5);
+  this.elevation = Math.floor(
+    ((Math.cos(row * 0.21 + 0.6) + Math.sin(col * 0.14 + 1.83) + 2) / 4) * 8 - 5);
 }
